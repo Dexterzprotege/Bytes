5b2882edd5d47e489ae37e745585a49ad060ea76
towseef41 <31309254+towseef41@users.noreply.github.com>
2021-05-28 21:48:48 +0530

    Add files via upload

---
+++ Towseef/28May.py
@@ -0,0 +1,41 @@
+# 1. (Search) Median of two sorted arrays: https://leetcode.com/problems/median-of-two-sorted-arrays/
+
+class Solution:
+    def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
+        m = len(nums1)
+        n = len(nums2)
+        resarr = [0]*(m+n)
+        i = 0
+        j = 0
+        k = 0
+        while i < m and j < n:
+            if nums1[i] > nums2[j]:
+                resarr[k] = nums2[j]
+                j += 1
+            else:
+                resarr[k] = nums1[i]
+                i += 1
+            k += 1
+        
+        while i < m:
+            resarr[k] = nums1[i]
+            i += 1
+            k += 1
+        
+        while j < n:
+            resarr[k] = nums2[j]
+            j += 1
+            k += 1
+        mid = len(resarr)//2
+        if len(resarr) == 0:
+            return float("{:.4f}".format(0))
+        elif len(resarr) == 1:
+            return float("{:.4f}".format(resarr[0]))
+        else:
+            if (len(resarr)%2 == 0):
+                return float("{:.4f}".format((resarr[mid-1]+resarr[mid])/2))
+            else:
+                return float("{:.4f}".format(resarr[mid]))
+                
+            
+        
