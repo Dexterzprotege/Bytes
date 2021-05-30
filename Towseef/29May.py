15b6387adce023c6a042ab2e7b00d43d7f1750e0
towseef41 <31309254+towseef41@users.noreply.github.com>
2021-05-30 21:06:38 +0530

    Update 29May.py

--- Towseef/29May.py
+++ Towseef/29May.py
@@ -27,4 +27,45 @@ class Solution:
                 end = len(nums)-i-1
                 break
         return [start, end]
+#https://leetcode.com/problems/find-first-and-last-position-of-element-in-sorted-array
+#O(log(n)) solution
+class Solution:
+    def searchRange(self, nums: List[int], target: int) -> List[int]:
+        start = -1
+        end = -1
+        lo = 0
+        hi = len(nums)-1
+        while lo <= hi:
+            mid = (lo+hi)//2
+            if nums[mid] == target:
+                if mid == 0:
+                    start = mid
+                    break
+                elif mid > 0 and nums[mid-1] != nums[mid]:
+                    start = mid
+                    break
+                elif mid > 0 and nums[mid-1] == nums[mid]:
+                    hi = mid - 1
+            elif nums[mid] < target:
+                lo = mid + 1
+            else:
+                hi = mid - 1
+        lo = 0
+        hi = len(nums)-1
+        while lo <= hi:
+            mid = (lo+hi)//2
+            if nums[mid] == target:
+                if mid == len(nums)-1:
+                    end = mid
+                    break
+                elif mid < len(nums)-1 and nums[mid+1] != nums[mid]:
+                    end = mid
+                    break
+                elif mid < len(nums)-1 and nums[mid+1] == nums[mid]:
+                    lo = mid + 1
+            elif nums[mid] < target:
+                lo = mid + 1
+            else:
+                hi = mid - 1    
+        return [start, end]
  
